73f081ba449104a1406edc75dab6b6800c13da15
Brent Roose <brent@spatie.be>
2020-07-07 10:08:33 +0200

    Google Analytics

--- resources/views/partials/gtm-body.blade.php
+++ resources/views/partials/gtm-body.blade.php
@@ -1,4 +1,10 @@
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQ3SBMQ"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-171834166-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-171834166-1');
+</script>
