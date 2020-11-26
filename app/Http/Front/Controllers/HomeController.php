ccc0143028d9f45b6ec5559044223c126513ee85
freek <freek@spatie.be>
2020-11-26 09:00:08 +0100

    wip

--- app/Http/Front/Controllers/HomeController.php
+++ app/Http/Front/Controllers/HomeController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Front\Controllers;
 
+use Spatie\PriceApi\SpatiePriceApi;
+
 class HomeController
 {
     public function __invoke()
     {
-        return view('front.home.index');
+        $purchasableId = config('services.spatie_prices_api.purchasable_id');
+
+        $prices = SpatiePriceApi::getPriceForPurchasable($purchasableId);
+
+        return view('front.home.index', [
+            'couldFetchPrice' => $prices['couldFetchPrice'],
+            'price' => $prices['actual'],
+            'priceWithoutDiscount' => $prices['withoutDiscount'],
+            'discount' => $prices['discount'],
+        ]);
     }
 }
--- resources/views/partials/priceCard.blade.php
+++ resources/views/partials/priceCard.blade.php
@@ -1,7 +1,3 @@
-@php
-    $coupon = \App\Support\Coupon::forCouponName('default')
-@endphp
-
 <section class="px-8 flex justify-center">
     <div class="max-w-md mx-auto xs:flex xs:w-full">
         <div class="z-10 xs:z-30 xs:fix-z -mt-6 -mx-4 flex-grow shadow-2xl">
@@ -9,43 +5,46 @@
             <div class="border-l border-r border-b border-gray-200 bg-white">
                 <div class="text-center py-4 leading-none">
                     <div class="font-display font-semibold text-3xl">
-                        @if($coupon->active())
-                            <div
-                                class="flex flex-col items-center mb-2 text-center text-green-500 uppercase text-xs tracking-widest leading-snug">
-                                <div>{{ $coupon->label() }} ending in</div>
+                        @if($couldFetchPrice)
+                            @if($discount->active)
                                 <div
-                                    class="z-10 transform rotate-0 bg-green-400 font-normal text-white px-1 py-1 shadow-md"
-                                    style="--transform-rotate: -1.5deg !important">
-                                    <x-countdown :expires="$coupon->expiresAt()">
+                                    class="flex flex-col items-center mb-2 text-center text-green-500 uppercase text-xs tracking-widest leading-snug">
+                                    <div>{{ $discount->name }} ending in</div>
+                                    <div
+                                        class="z-10 transform rotate-0 bg-green-400 font-normal text-white px-1 py-1 shadow-md"
+                                        style="--transform-rotate: -1.5deg !important">
+                                        <x-countdown :expires="$discount->expiresAt()">
                                         <span class="bg-green-500 px-1"><span
                                                 x-text="timer.days">{{ $component->days() }}</span> days</span>
-                                        <span class="bg-green-500 px-1"><span
-                                                x-text="timer.hours">{{ $component->hours() }}</span> hours</span>
-                                        <span class="bg-green-500 px-1"><span
-                                                x-text="timer.minutes">{{ $component->minutes() }}</span> minutes</span>
-                                    </x-countdown>
+                                            <span class="bg-green-500 px-1"><span
+                                                    x-text="timer.hours">{{ $component->hours() }}</span> hours</span>
+                                            <span class="bg-green-500 px-1"><span
+                                                    x-text="timer.minutes">{{ $component->minutes() }}</span> minutes</span>
+                                        </x-countdown>
+                                    </div>
                                 </div>
-                            </div>
+                            @endif
                         @endif
                         Videos & ebook
                     </div>
 
-                    <div class="flex justify-center mt-6">
-                        <div class="font-display">
-                            <sup class="text-gray-500 text-3xl" data-id="current-currency-{{ config('services.paddle.product_id') }}"></sup><span
-                                class="font-bold text-5xl" data-id="current-price-{{ config('services.paddle.product_id') }}">—</span>
-                            <span data-id="original-display-{{ config('services.paddle.product_id') }}" class="hidden absolute right-full mr-4 top-0 mt-2">
-                                <sup class="text-gray-500 text-xs" data-id="original-currency-{{ config('services.paddle.product_id') }}"></sup><span
-                                    class="text-gray-500 line-through" data-id="original-price-{{ config('services.paddle.product_id') }}">—</span>
-                            </span>
-                        </div>
-                    </div>
 
-                    @if ($coupon->active())
-                        <div class="mt-6 text-gray-500 text-xs">
-                            {{ $coupon->percentage() }}% off with coupon <code class="px-2 py-1 text-gray-700 bg-gray-100 bg-opacity-25">{{ $coupon->code() }}</code>
-                        </div>
-                    @endif
+                    <div class="flex justify-center mt-6">
+                        @if($couldFetchPrice)
+                            <div class="font-display">
+                                <span
+                                    class="font-bold text-5xl"
+                                    data-id="current-price-{{ config('services.paddle.product_id') }}">{{ $price->formattedPrice() }}</span>
+                                @if($discount->active)
+                                    <span class="absolute right-full mr-4 top-0 mt-2">
+                                        <span class="text-gray-500 line-through">
+                                            {{ $priceWithoutDiscount->formattedPrice() }}
+                                         </span>
+                                    </span>
+                                @endif
+                            </div>
+                        @endif
+                    </div>
                 </div>
                 <div class="text-center z-10 -mb-3">
                     <a href="https://spatie.be/products/laravel-beyond-crud">
@@ -76,52 +75,13 @@
                             Prices exclusive of VAT for buyers without a valid VAT number.
                             <br>
                             <br>
-                            We use purchasing power parity provided by Paddle.
+                            We use purchasing power parity.
                             <br>
                             <a class="underline" href="mailto:info@spatie.be?subject=CRUD%20for%20students">Contact
-                                us</a> if your currency is <a class="underline" target="_blank"
-                                                              href="https://paddle.com/support/what-currencies-do-you-support/">not
-                                supported</a> or if you are a student.
+                                us</a> if you are a student.
                         </p>
                     </div>
                 </div>
             </div>
         </div>
 </section>
-
-<script type="text/javascript">
-    function indexOfFirstDigitInString(string) {
-        let firstDigit = string.match(/\d/);
-
-        return string.indexOf(firstDigit);
-    }
-
-    function displayPaddleProductPrice(productId) {
-        Paddle.Product.Prices(productId, function(prices) {
-            let priceString = prices.price.net;
-
-            let factor = {{ $coupon->active() ? (100 - $coupon->percentage())/100 : 1 }};
-
-            let indexOFirstDigitInString = indexOfFirstDigitInString(priceString);
-
-            let price = priceString.substring(indexOFirstDigitInString);
-            price = price.replace('.00', '').replace(/,/g, '');
-
-            let currencySymbol = priceString.substring(0, indexOFirstDigitInString);
-            currencySymbol = currencySymbol.replace('US', '');
-
-            document.querySelector(`[data-id="original-currency-${productId}"]`).innerHTML = currencySymbol;
-            document.querySelector(`[data-id="original-price-${productId}"]`).innerHTML = price;
-
-            document.querySelector(`[data-id="current-currency-${productId}"]`).innerHTML = currencySymbol;
-            document.querySelector(`[data-id="current-price-${productId}"]`).innerHTML = Math.ceil(price * factor);
-            
-            if(factor < 1) {
-                document.querySelector(`[data-id="original-display-${productId}"]`).classList.remove('hidden');
-            }
-        });
-    }
-
-    displayPaddleProductPrice({{ config('services.paddle.product_id') }});
-
-</script>
